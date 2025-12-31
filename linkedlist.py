a2ccf6dd88cc9e330093bf422a4fbe013ee4bcec
devAmjad4590 <developeramjad4590@gmail.com>
2025-12-31 15:35:36 +0300

    kth element implementation

--- linkedlist.py
+++ linkedlist.py
@@ -151,11 +151,30 @@ class LinkedList:
             temp.next = before
             before = temp
             temp = after
-linkedlist = LinkedList(4)
-linkedlist.append(9)
-linkedlist.pop()
-linkedlist.pop()
-linkedlist.append(10)
-linkedlist.printList()
+
+def find_kth_from_end(ll, k):
+    slow = ll.head
+    fast = ll.head
+    
+    for _ in range(k):
+       if fast is None:
+           return None
+       fast = fast.next
+        
+    while fast:
+       slow = slow.next
+       fast = fast.next
+
+    return slow
+    
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
 
 
+k = 2
+result = find_kth_from_end(my_linked_list, k)
+print(result.value)
